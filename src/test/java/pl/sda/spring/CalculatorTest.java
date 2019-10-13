406dfbe466a0000b800f23f879520106db236bf6
akmar <akmarpol@gmail.com>
2019-10-13 11:11:02 +0200

    Update

--- src/test/java/pl/sda/spring/CalculatorTest.java
+++ src/test/java/pl/sda/spring/CalculatorTest.java
@@ -27,4 +27,65 @@ public class CalculatorTest {
         //then
         Assert.assertEquals(5.0, actual, 0.00000001);
     }
+
+    @Test
+    public void shouldReturnMinusOneForGivenArgumentsOnSubstractionOperation() {
+        //given
+        double arg1 = 2.0;
+        double arg2 = 3.0;
+
+        //when
+        double actual = calculator.calculate(OperationType.SUBTRACTION, arg1, arg2);
+
+        //then
+        Assert.assertEquals(-1, actual, 0.00000001);
+    }
+    @Test
+    public void shouldReturnSixForGivenArgumentsOnMultiplicationOperation() {
+        //given
+        double arg1 = 2.0;
+        double arg2 = 3.0;
+
+        //when
+        double actual = calculator.calculate(OperationType.MULTIPLICATION, arg1, arg2);
+
+        //then
+        Assert.assertEquals(6, actual, 0.00000001);
+    }
+    @Test
+    public void shouldReturnOneForGivenArgumentsOnMDivisionOperation() {
+        //given
+        double arg1 = 3.0;
+        double arg2 = 3.0;
+
+        //when
+        double actual = calculator.calculate(OperationType.DIVISION, arg1, arg2);
+
+        //then
+        Assert.assertEquals(1, actual, 0.00000001);
+    }
+    @Test
+    public void shouldReturnTenForGivenArgumentsOnPercentageOperation() {
+        //given
+        double arg1 = 10.0;
+        double arg2 = 100.0;
+
+        //when
+        double actual = calculator.calculate(OperationType.PERCENTAGE, arg1, arg2);
+
+        //then
+        Assert.assertEquals(10, actual, 0.00000001);
+    }
+    @Test
+    public void shouldReturnFourForGivenArgumentsOnPowerOperation() {
+        //given
+        double arg1 = 2.0;
+        double arg2 = 2.0;
+
+        //when
+        double actual = calculator.calculate(OperationType.POWER, arg1, arg2);
+
+        //then
+        Assert.assertEquals(4, actual, 0.00000001);
+    }
 }
